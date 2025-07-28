89143b137b698f91968483bd9f8623b6142e10bd
menblox <menblox@mail.ru>
2025-07-28 20:59:42 +0300

    подправил black

--- app/auth.py
+++ app/auth.py
@@ -73,5 +73,6 @@ def verify_token(token: str) -> Name_JWT:
 
     except JWTError:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, 
-            detail="Could not validate credentials") from JWTError
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+        ) from JWTError
